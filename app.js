31d8ff21607a51fac93a1346813ca6558fbdbe8e
Jonathan Herman <jdh11235@gmail.com>
2015-02-02 17:35:39 -0600

    stuff

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+window.addEventListener('load', function () {
+	'use strict';
+	var $textbox = document.getElementById('input');
 
+	window.jdh = {
+		hello: function (input) {
+			alert('Hello ' + input + '!');
+		}
+	};
+
+	function update (input) {
+		jdh.hello(input);
+	}
+
+	function textbox_keydown (event) {
+		if (event.keyCode === 13) {
+			var text = event.target.innerText;
+			update(text);
+			event.target.innerText = text;
+		}
+	}
+
+	$textbox.addEventListener('keydown', textbox_keydown);
+});
