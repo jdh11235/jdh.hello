354e02fe2910953f535555960ba3225610068b04
Jonathan Herman <jdh11235@gmail.com>
2015-02-03 03:50:32 -0600

    fix for lack of .innerText in FireFox

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 window.addEventListener('load', function () {
 	'use strict';
+
+	/* fix for lack of .innerText in FireFox */
+	window.setText = function (element, text) {
+		if(document.all){
+			element.innerText = text;
+		} else{
+			element.textContent = text;
+		}
+	};
+
+	window.getText = function (element) {
+		if(document.all){
+			return element.innerText;
+		} else{
+			return element.textContent;
+		}
+	}
+
 	var $textbox = document.getElementById('input');
 
 	window.jdh = {
@@ -14,14 +32,14 @@ window.addEventListener('load', function () {
 
 	function textbox_handler (event) {
 		if (event.keyCode === 13) {
-			var text = event.target.innerText.replace(/\n/gi, '');
-			event.target.innerText = text;
+			var text = getText(event.target).replace(/\n/gi, '');
+			setText(event.target, text);
 			update(text);
 		}
 	}
 
 	$textbox.addEventListener('keyup', textbox_handler);
-	$textbox.innerText = $textbox.innerText.replace(/\n/gi, '');
+	setText($textbox, getText($textbox).replace(/\n/gi, ''));
 
 	document.addEventListener('touchmove', function (event) {
 		event.preventDefault();
